eeccffd72c838a819d5a1c47c5e1a5ff47088ecf
dltjwls02 <102820056+dltjwls02@users.noreply.github.com>
2025-07-26 15:35:01 +0900

    Feature/develop (#47)
    * 배포 설정
    
    * test
    
    * Update

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const address = process.env.ADDRESS;
 
 // CORS 설정
 const corsOptions = {
@@ -27,14 +28,18 @@ const corsOptions = {
       callback(null, true);
       return;
     }
+    console.log('start: 배포 연결 실행');
 
     // 프로덕션 환경에서는 허용된 도메인만
     const allowedOrigins = [
-      'https://your-frontend-domain.com', // 실제 프론트엔드 도메인으로 변경
-      'https://onairmate.vercel.app', // 예시 도메인
+      address,
+      //'https://your-frontend-domain.com', // 실제 프론트엔드 도메인으로 변경
+      //'https://onairmate.vercel.app', // 예시 도메인
       'http://localhost:3000', // 로컬 개발용
       'http://localhost:3001', // 로컬 개발용
     ];
+    console.log('배포 주소', address);
+    console.log('연결 origin:', origin);
 
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
